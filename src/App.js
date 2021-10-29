ee3564a75e4f76c98e3186e36ed6d3f8a1c82013
Kenny Quach <kennyquach60@gmail.com>
2021-10-29 16:34:43 -0700

    Add useState

--- src/App.js
+++ src/App.js
@@ -10,24 +10,31 @@ function App() {
         lastName = 'Sparrow'
         age = {30}
         hairColor = 'black'
+        initialWeight = {150}
       />
       <Person 
         firstName = 'Orlando' 
         lastName = 'Bloom'
         age = {29}
         hairColor = 'brown'
+        initialWeight = {170}
+
       />
       <Person 
         firstName = 'Dembe' 
         lastName = 'Zuma'
         age = {33}
         hairColor = 'blonde'
+        initialWeight = {165}
+
       />
       <Person 
         firstName = 'Raymond' 
         lastName = 'Reddington'
         age = {40}
         hairColor = 'Gray'
+        initialWeight = {180}
+
       />
     </div>
   );
--- src/components/Personcard.js
+++ src/components/Personcard.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const Person = (props)=> {return (
-    <div>
-        <h2>{props.lastName}, {props.firstName}</h2>
-        <p>Age: {props.age}</p>
-        <p>Hair color: {props.hairColor}</p>
-    </div>
-)}
+const Person = (props)=> {
+    const [weight, setWeight] = useState(props.initialWeight)
+    return (
+        <div>
+            <h2>{props.lastName}, {props.firstName}</h2>
+            <p>Age: {props.age}</p>
+            <p>Hair color: {props.hairColor}</p>
+            <p>Weight: {weight}</p>
+            <button onClick = {(event) => setWeight(weight - 1)}>Lose Weight</button>
+        </div>
+    )
+}
 
 export default Person
