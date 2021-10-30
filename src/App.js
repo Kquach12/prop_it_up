67b8eec7b517f2703738b2ad1e0e44cf7f318723
Kenny Quach <kennyquach60@gmail.com>
2021-10-29 17:55:44 -0700

    Add birthday button

--- src/App.js
+++ src/App.js
@@ -36,6 +36,7 @@ function App() {
         initialWeight = {180}
 
       />
+      
     </div>
   );
 }
--- src/components/Personcard.js
+++ src/components/Personcard.js
@@ -2,13 +2,16 @@ import React, {useState} from 'react'
 
 const Person = (props)=> {
     const [weight, setWeight] = useState(props.initialWeight)
+    const [age, setAge] = useState(props.age)
     return (
         <div>
             <h2>{props.lastName}, {props.firstName}</h2>
-            <p>Age: {props.age}</p>
+            <p>Age: {age}</p>
             <p>Hair color: {props.hairColor}</p>
             <p>Weight: {weight}</p>
-            <button onClick = {(event) => setWeight(weight - 1)}>Lose Weight</button>
+            <button onClick = {(event) => setWeight(weight - 1)}>Lose Weight</button> 
+            <br/>
+            <button onClick = {(event) => setAge(age+1)}>It's your birthday!</button>
         </div>
     )
 }
